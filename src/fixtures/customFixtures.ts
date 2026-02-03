c613c489b30b501f1a748cb0ca0067374aceee0e
YaraYev <yevayara@gmail.com>
2026-02-03 13:16:44 +0200

    small amends

--- src/fixtures/customFixtures.ts
+++ src/fixtures/customFixtures.ts
@@ -16,5 +16,3 @@ export const test = base.extend<Fixtures>({
         await use(new ProcessDashboardPage(page));
     },
 });
-
-export { expect } from '@playwright/test';
--- tests/verifyAction.spec.ts
+++ tests/verifyAction.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '../src/fixtures/customFixtures';
+import { expect } from '@playwright/test';
+import { test } from '../src/fixtures/customFixtures';
 import { faker } from '@faker-js/faker';
 
 test('Verifying adding and saving of action process', async ({
@@ -8,14 +9,12 @@ test('Verifying adding and saving of action process', async ({
   const processName = faker.word.sample({ length: 5 });
 
   await homePage.navigate();
-
   await expect(homePage.title, 'Home page title should be "Create dashboard sequence"').toHaveText('Create dashboard sequence');
 
   await homePage.clickSideBarProcesses();
   await expect(processDashboardPage.createProcessBtn, 'Expected "Create process" button to be visible after clicking "Processes"').toBeVisible();
 
   const processDesignerPage = await processDashboardPage.createProcess();
-
   await processDesignerPage.fillProcessName(processName);
 
   await processDesignerPage.openActionsSidebar();
@@ -23,7 +22,6 @@ test('Verifying adding and saving of action process', async ({
   await processDesignerPage.closeActionsSidebar();
 
   await processDesignerPage.save();
-
   await expect(processDesignerPage.saveIconOk,
     'Expected success icon to appear after saving the process').toBeVisible();
 
